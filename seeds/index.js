875e7432341959099dce846853b6ff675ab2bce8
HyRaXxItY <yasirmohammed122002@gmail.com>
2022-03-07 02:08:22 +0530

    style

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,14 @@ const seedDB = async () =>{
     await Campground.deleteMany({});
     for( let i=0; i<50; i++){
         const random1000 = Math.floor(Math.random()*1000)
+        const price = Math.floor(Math.random()*30) + 1;
         const camp = new Campground({
             location:`${cities[random1000].city}  ${cities[random1000].state}`,
-            title: `${sample(descriptors)}  ${sample(places)}`
+            title: `${sample(descriptors)}  ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nostrum facilis dolores tempore consectetur minima laudantium provident possimus mollitia eaque, fuga iusto quos perferendis voluptas beatae inventore fugit quidem? Asperiores?',
+            price: price,
+            image: 'http://source.unsplash.com/collection/483251' 
+
         })
         await camp.save();
     }
