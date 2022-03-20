422abecb02cb3645ce027e0c1d48edcf6eec1fe7
HyRaXxItY <yasirmohammed122002@gmail.com>
2022-03-21 00:45:38 +0530

    advGuild

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id);
+    const campground = await Campground.findById(id).populate('reviews');
     // res.render('campgrounds/show', { camp })
     res.render('campgrounds/show', { campground })
 }))
@@ -122,6 +122,18 @@ app.post('/campgrounds/:id/review', validateReview, catchAsync(async (req, res) 
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 
+
+}));
+
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    // pull--> literally pulls the value from the array ( here it pulls reviews array element having 
+    // element id as reviewId )
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
 
 }))
 
--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -12,6 +12,17 @@ const CampgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Review'
     }]
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (del) {
+    if (del) {
+        await Review.deleteMany({
+            _id: {
+                $in: del.reviews   // specifying that, id is somewhere ''in'' del.reviews object 
+                //(its array , we have defined reviews array here remember ? )
+            }
+        })
+    }
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
