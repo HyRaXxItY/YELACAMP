581e2a113614f81428cc6b884bc2ccb3f09c6688
HyRaXxItY <yasirmohammed122002@gmail.com>
2022-04-09 20:15:40 +0530

    seperate folder for image in cloudinary

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ const sessionConfig = {
     resave: false,
     saveUninitialized: true,
     cookie: {
-        //secure: true,
+        secure: true,
         httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 1,
         maxAge: 1000 * 60 * 60 * 24 * 7
