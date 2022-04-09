db004eab150b1c6f2f14caafff66be7e7c81d7fe
HyRaXxItY <yasirmohammed122002@gmail.com>
2022-04-09 17:00:53 +0530

    deploy wth secr

--- app.js
+++ app.js
@@ -128,7 +128,9 @@ app.use((err, req, res, next) => {
     res.status(status_code).render('error/error', { err });
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
 
-    console.log("serving on port 3000");
+app.listen(port, () => {
+
+    console.log(`serving on port ${port}`);
 })
